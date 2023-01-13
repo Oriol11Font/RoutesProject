594ef2dd175a69f411e4cb977bf09e1bad168f3e
ofont <o.font@ratenow.es>
2023-01-13 10:33:16 +0100

    app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
+import { HelloComponent } from './hello/hello.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
-
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'hello/:name', component: HelloComponent},
+  {path: '**', component: ErrorComponent}
+];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { RouterModule, Routes } from '@angular/router'
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ErrorComponent } from './error/error.component';
 import { HelloComponent } from './hello/hello.component';
 import { HomeComponent } from './home/home.component';
 
-const appRoutes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'home', component: HomeComponent},
-  {path: 'hello/:name', component: HelloComponent},
-  {path: '**', component: ErrorComponent}
-];
+
 
 @NgModule({
   declarations: [
@@ -23,7 +18,7 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
